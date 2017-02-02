6f3aedf8061a2a188521989553cf740111eabd53
Denis <C:\Users\nikolay\AppData\Roaming\The Bat!>
2017-02-02 16:30:42 +0300

    doubly-linked-list/feature: make constructor, append, head,tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,42 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        var tail = new Node(null, null, null);
+        var head = new Node(null, null, tail);
+        this._tail = tail;
+        this._tail.prev = head;
+        this._head = head;
+        this.length = 0;
+        return this;
+    }
 
-    head() {}
+    append(data) {
+        var newNode = new Node(data);
+        var currentTail = this._tail;
+        if (this.length) {
+          //  currentTail.next = newNode;
+            this._tail = newNode;
+            this._tail.prev = currentTail;
+            this._tail.next = newNode;
+        } else {
+            //this._head.data = data;
+            this._head = newNode;
+            this._tail = newNode;
+        }
+        this.length++;
 
-    tail() {}
+        return newNode;
+    }
+
+    head() {
+        return this._head.data;
+    }
+
+    tail() {
+        return this._tail.data;
+    }
 
     at(index) {}
 
