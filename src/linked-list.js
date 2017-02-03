8ebf7517d6b7e5d5c21ef52e07f5e371564de96b
Denis <C:\Users\nikolay\AppData\Roaming\The Bat!>
2017-02-03 17:38:01 +0300

    doubly-linked-list/feature: complied  deleteAt and reverse

--- src/linked-list.js
+++ src/linked-list.js
@@ -41,7 +41,7 @@ class LinkedList {
     at(index) {
         let i = 0;
         let currentHead = this._head;
-        if(index > this.length) {
+        if(index > this.length && index >= 0) {
             return currentHead.data;
         }else{
             while (index > i){
@@ -68,8 +68,6 @@ class LinkedList {
             this._head.prev = NewNode;
             this._head = NewNode;
         }
-
-
         return this;
     }
 
@@ -85,11 +83,45 @@ class LinkedList {
         return this;
     }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        let currentHead = this._head;
+        let currentTail = this._tail;
+        let i=0;
+        while (i != index){
+            currentHead = currentHead.next;
+            i++;
+        }
+        if (index == 0) {
+            this._head = currentHead.next;
+            currentHead.prev = null;
+        }else if (index === this.length) {
+            this._tail = currentTail.prev;
+            currentTail.next = null;
+        } else {
+            currentHead.prev.next = currentHead.next; //tail
+            currentHead.next.prev = currentHead.prev; //head
+        }
+        this.length--;
+        return this;
+    }
 
-    reverse() {}
+    reverse() {
+        let currentHead = this._head;
+        let currentTail = this._tail;
+        let NewNode = new Node();
+        for (let i=0; i<this.length/2; i++){
+            NewNode.data = currentHead.data;
+            currentHead.data = currentTail.data;
+            currentTail.data = NewNode.data;
+            currentHead = currentHead.next;
+            currentTail = currentTail.prev;
+        }
+        return this;
+    }
 
-    indexOf(data) {}
+    indexOf(data) {
+
+    }
 }
 
 module.exports = LinkedList;
