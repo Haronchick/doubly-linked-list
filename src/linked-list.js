96207ce0b068616b88ac01c4df7044bfc7f91f75
Denis <C:\Users\nikolay\AppData\Roaming\The Bat!>
2017-02-03 15:22:08 +0300

    doubly-linked-list/feature: edit constuctor, append; implement at, insertAt, isEmpty

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,32 +3,32 @@ const Node = require('./node');
 class LinkedList {
 
     constructor() {
-        var tail = new Node(null, null, null);
-        var head = new Node(null, null, tail);
-        this._tail = tail;
-        this._tail.prev = head;
-        this._head = head;
+
+        this._tail = null;
+        // this._tail.prev = head;
+        this._head = null;
         this.length = 0;
-        return this;
+        //return this;
     }
 
     append(data) {
-        var newNode = new Node(data);
-        var currentTail = this._tail;
+        let newNode = new Node(data);
+        let currentTail = this._tail;
         if (this.length) {
-          //  currentTail.next = newNode;
+            newNode.prev = this._tail;
+            currentTail.next = newNode;
             this._tail = newNode;
-            this._tail.prev = currentTail;
-            this._tail.next = newNode;
+
         } else {
             //this._head.data = data;
             this._head = newNode;
             this._tail = newNode;
         }
         this.length++;
-
-        return newNode;
+        return this;
     }
+
+
 
     head() {
         return this._head.data;
@@ -38,13 +38,52 @@ class LinkedList {
         return this._tail.data;
     }
 
-    at(index) {}
+    at(index) {
+        let i = 0;
+        let currentHead = this._head;
+        if(index > this.length) {
+            return currentHead.data;
+        }else{
+            while (index > i){
+                currentHead = currentHead.next;
+                i++;
+            }
+         return currentHead.data;
+        }
 
-    insertAt(index, data) {}
+    }
 
-    isEmpty() {}
+    insertAt(index, data) {
+        let NewNode = new Node(data);
+        let currentHead = this._head;
+        var i = 0;
+        if (index){
+            while (index > i){
+                currentHead = currentHead.next;
+                i++
+            }
+            currentHead.data = NewNode.data;
+        }else{
+            NewNode.next = this._head;
+            this._head.prev = NewNode;
+            this._head = NewNode;
+        }
 
-    clear() {}
+
+        return this;
+    }
+
+    isEmpty() {
+        if (this.length) { return false; }
+        else{ return true; }
+    }
+
+    clear() {
+        this._tail.data = null;
+        this._head.data = null;
+        this.length = 0;
+        return this;
+    }
 
     deleteAt(index) {}
 
